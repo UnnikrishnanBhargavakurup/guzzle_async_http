eef4d87cfd5fb224df0f554a5c2df5a81bebfb34
Unnikrishnan.B <unnikrishnanadoor@gmail.com>
2021-06-24 17:04:22 +0530

    added docker for api server and the CLI app

---
+++ app/asyn_req.php
@@ -0,0 +1,21 @@
+<?php
+
+include('./vendor/autoload.php');
+
+use GuzzleHttp\Client;
+
+$promises = [];
+$client = new Client();
+$start = time();
+for($i = 1; $i <= 10; $i++) {
+    $url = 'http://localhost/?id=' . $i;
+    print("request :  ". $i . PHP_EOL);
+    $promises[] = $client->requestAsync('GET', $url)->then(function ($response) use ($i) {
+       print("response :  ". $i . " : ". $response->getBody()->getContents() . PHP_EOL);
+    });
+}
+
+
+\GuzzleHttp\Promise\all($promises)->wait();
+
+print "Total time take to complete 10 requet " . time() - $start;
