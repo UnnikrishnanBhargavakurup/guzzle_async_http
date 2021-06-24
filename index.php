98dec7f5e227f4bf5a65e30986256cd3d52688e5
Ramy hakam <pencilsoft1@gmail.com>
2021-06-24 11:11:18 +0200

    Compose-support

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 
-if(!empty($_GET) && isset($_GET['id'])) {
-  $requestId = intval($_GET['id']);
-  $start = time();
-  $delay = 11 - $requestId;
-  sleep($delay);
-  header('Content-Type: application/json');
-  print json_encode(["sleep" => $delay . " seconds"]);
-  exit();
+include('./vendor/autoload.php');
 
+use GuzzleHttp\Client;
+
+$promises = [];
+$client = new Client();
+for($i = 1; $i <= 10; $i++) {
+    $url = 'http://host.docker.internal:8000/api/v1/ping';
+    $requestTime = time();
+    $promises[] = $client->requestAsync('GET', $url)->then(function ($response) use ($i,$requestTime) {
+        echo sprintf ("Request number %s started at %s AND ended at %s request duration is %s  with External API  delay = %s ",$i,$requestTime,time(),time() - $requestTime,$response->getBody());
+        echo "<br>";
+    });
 }
 
-print "Please provide a request id";
+
+\GuzzleHttp\Promise\all($promises)->wait();
